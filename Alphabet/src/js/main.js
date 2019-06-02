fbfd4e18a0184bda317580e846f7b2489d6c64bb
dquizi <quizi@bk.ru>
2019-06-02 17:24:44 +0300

    Добавил счетчик на кнопку

--- Alphabet/src/js/main.js
+++ Alphabet/src/js/main.js
@@ -1,11 +1,12 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
-var close = document.querySelector('#close')
+var close = document.querySelector('#close');
+const openModal = () => modal.classList.add('modal_active');
+const closeModal = () => modal.classList.remove('modal_active');
 
-button.addEventListener('click', function() {
-modal.classList.add('modal_active');
+button.addEventListener('click', () => {
+	openModal();
+	setTimeout(() => closeModal(), 5000)
 });
 
-close.addEventListener('click', function() {
-	modal.classList.remove('modal_active');
-});
+close.addEventListener('click', closeModal);
